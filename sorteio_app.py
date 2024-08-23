eb26886bf78ffa690622bfb3570f5bae6e7b1fd5
jennifermel <jennifermelelissa8503@gmail.com>
2024-08-23 15:08:05 -0300

    Comentario sobre o -1

--- sorteio_app.py
+++ sorteio_app.py
@@ -17,6 +17,7 @@ while True:
             print(f'{nome} inserido com sucesso.')
             continue
         case '2':
+            #coloca o -1 pois o len vai retornar 1 numero a mais, se o numero da lista foi 10 ele retorna 10, mas nao contabiliza o 0 do indice, por isso o  menos -1, para que fique exato o tanto de nomes cadastrados, e nao um a mais
             indice_sorteado = random.randint(0,len(nomes)-1)
             print(f'Nome sorteado: {nomes[indice_sorteado]}.')
         case '3':
